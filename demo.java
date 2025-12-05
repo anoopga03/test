6ca49d9d6a8b371f8e859a82be9a8d9426ec3be5
Anoop <anoopga3@gmail.com>
2025-12-05 11:25:23 +0530

    first commit

---
+++ demo.java
@@ -0,0 +1,35 @@
+package collections;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashmapDemo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		HashMap<String, String> config = new HashMap<>();
+		
+		config.put("browser", "chrome");
+		config.put("url", "https://google.com");
+		config.put("timeout", "30");
+		config.put("headless", "false");
+		config.put("browser", "firefox");
+		
+		System.out.println("Entire config : " + config);
+		
+		System.out.println("Value of url : " + config.get("url"));
+		
+		System.out.println("Does timeout exist? " + config.containsKey("timeout"));
+		
+		config.remove("headless");
+		System.out.println("Updated config : " + config);
+		
+		System.out.println("\nLoop using entry set");
+		for(Map.Entry<String, String> entry : config.entrySet()) {
+			System.out.println(entry.getKey() + " : " + entry.getValue());
+		}
+		
+	}
+
+}
